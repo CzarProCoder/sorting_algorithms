f4f6b9425f1ed58bc8e0f15b0a39d4a9f5929942
CzarProCoder <juliusczar36@gmail.com>
2023-05-01 13:21:01 +0300

    Wrote a function that sorts an array of integers in ascending order using the Bitonic sort algorithm

--- sort.h
+++ sort.h
@@ -60,4 +60,12 @@ void heap_sort(int *array, size_t size);
 
 void radix_sort(int *array, size_t size);
 
+void swap_ints(int *a, int *b);
+
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow);
+
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+
+void bitonic_sort(int *array, size_t size);
+
 #endif /* SORT_H */
