ee13b7b27a9d99b4d76fa770efe7b290725b6413
CzarProCoder <juliusczar36@gmail.com>
2023-05-01 13:06:18 +0300

    Wrote a function that sorts an array of integers in ascending order using the Heap sort algorithm

--- sort.h
+++ sort.h
@@ -52,4 +52,10 @@ void rec_merge(int *array, int *sortArr, size_t l, size_t r);
 
 void merge_sort(int *array, size_t size);
 
+void swap_ints(int *a, int *b);
+
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+
+void heap_sort(int *array, size_t size);
+
 #endif /* SORT_H */
